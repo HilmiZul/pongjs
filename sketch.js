b50b8a1b8c400dcccd03d1edb2a3d054d244ba8d
hilmizul <netspytux@gmail.com>
2026-06-23 19:53:59 +0700

    update major: UI, logic and control

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 let video;
 let poseNet;
-let poses = [];
+let hands = [];
 let pose;
 let leftWrist;
 let rightWrist;
@@ -12,21 +12,35 @@ let wMeja;
 let hMeja;
 let skorbar = 50;
 let gamePlay = false;
+let gameOver = false
+let gameWin = {
+  right: false,
+  left: false
+}
+let poin = 11
 
+function preload() {
+  poseNet = ml5.handPose({ flipped: true });
+}
 
 function setup() {
 	//createCanvas(950, 550);
 	createCanvas(windowWidth - 50, windowHeight - 150);
 
-	//video = createCapture(VIDEO);
-  //video.size(width, height);
+  video = createCapture(VIDEO, { flipped: true });
+  video.size(width, height);
+  video.hide();
+  poseNet.detectStart(video, modelReady);
 
-  //poseNet = ml5.poseNet(video, modelReady);
-  //poseNet.on("pose", function (results) {
-   // poses = results;
-  //});
+	// video = createCapture(VIDEO);
+	//  video.size(width, height);
+	//
+	//  poseNet = ml5.poseNet(video, modelReady);
+	//  poseNet.on("pose", function (results) {
+	//    poses = results;
+	//  });
 
-  //video.hide();
+  // video.hide();
 
 	wMeja = width;
 	hMeja = height;
@@ -36,9 +50,9 @@ function setup() {
 	bola = new Ball()
 }
 
-//function modelReady() {
-//  select("#status").html("");
-//}
+function modelReady(results) {
+  hands = results;
+}
 
 function game() {
 	// bola
@@ -52,12 +66,26 @@ function game() {
 	player_kiri.goal(bola);
 	player_kiri.cekTepi();
 
+  // check if skor = 11 poin sesuai rule pingpong
+  if(player_kiri.skor == poin) {
+    gameOver = true
+    gameWin.left = true
+    gameWin.right = false
+  }
+
 	// player kanan
 	player_kanan.show()
 	player_kanan.gerak();
 	player_kanan.nepak(bola);
 	player_kanan.goal(bola);
 	player_kanan.cekTepi();
+
+  // check if skor = 11 poin sesuai rule pingpong
+  if(player_kanan.skor == poin) {
+    gameOver = true
+    gameWin.left = false
+    gameWin.right = true
+  }
 }
 
 function meja() {
@@ -68,14 +96,14 @@ function meja() {
 	rect(0, 0 + skorbar, wMeja, hMeja - skorbar);
 	line(wMeja / 2, 0 + skorbar, wMeja / 2, hMeja);
 	strokeWeight(3);
-	line(0, hMeja / 2 + skorbar / 2, wMeja, hMeja / 2 + skorbar / 2);
-	if (!gamePlay) {
+	// line(0, hMeja / 2 + skorbar / 2, wMeja, hMeja / 2 + skorbar / 2);
+	if (!gamePlay && hands.length > 0) {
 		push();
 		fill(255);
 		noStroke();
 		textSize(20);
 		textAlign(CENTER, CENTER)
-		text("TEKAN SPASI", width / 2, 30);
+		text("TEKAN SPASI UNTUK MAIN", width / 2, 30);
 		pop();
 	}
 	pop();
@@ -91,6 +119,15 @@ function skor() {
 	pop();
 }
 
+function gamePause() {
+  noLoop()
+}
+
+function gameStart() {
+  noLoop()
+  loop()
+}
+
 function draw() {
 	background(100, 100, 250);
 	//image(video, 0, 0, width, height)
@@ -99,14 +136,53 @@ function draw() {
 	meja();
 	skor();
 
-	bola.show();
-	player_kiri.show();
-	player_kanan.show();
+  bola.show();
 
+  if(hands.length > 0) {
+    player_kiri.show();
+    player_kanan.show();
+  } else {
+    push()
+    fill(255, 255, 255)
+    strokeWeight(2);
+    stroke(0, 0, 0)
+    textSize(100);
+    textAlign(CENTER, CENTER)
+    text('Angkat Kedua Tanganmu!', width/2, height/2)
+    pop()
+  }
 
 	if (gamePlay) {
 		game();
 	}
+
+  if(gameOver && gameWin.left) {
+    noLoop()
+    push()
+    background(0, 0, 0, 200)
+    // translate(width, 0)
+    // scale(-1, 1)
+    textSize(60)
+    textAlign(CENTER, CENTER)
+    text("Selamat Player Kiri Menang!", width / 2, height / 2)
+    textSize(50)
+    textAlign(CENTER, BOTTOM)
+    text("Tekan ENTER untuk Permainan Baru", width / 2, height - 200)
+    pop()
+  } else if(gameOver && gameWin.right) {
+    noLoop()
+    push()
+    background(0, 0, 0, 200)
+    // translate(width, 0)
+    // scale(-1, 1)
+    textSize(60)
+    textAlign(CENTER, CENTER)
+    text("Selamat Player Kanan Menang!", width / 2, height / 2)
+    textSize(50)
+    textAlign(CENTER, BOTTOM)
+    text("Tekan ENTER untuk Permainan Baru", width / 2, height - 200)
+    pop()
+  }
 }
 
 function keyPressed() {
@@ -114,7 +190,17 @@ function keyPressed() {
 	if (key === "s") player_kiri.bawah = true;
 	if (key === "i") player_kanan.atas = true;
 	if (key === "k") player_kanan.bawah = true;
-	if (key === " ") gamePlay = true;
+	if (key === " ") gamePlay = true
+	if (keyCode === RETURN) {
+    gamePlay = false
+    gameOver = false
+    gameWin.left = false
+    gameWin.right = false
+    player_kanan.skor = 0
+    player_kiri.skor = 0
+    bola.reset()
+    loop()
+  }
 	if (keyCode === ESCAPE) gamePlay = false;
 }
 
